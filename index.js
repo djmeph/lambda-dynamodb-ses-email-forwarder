e25700ecb4d97d633c3740d9ffcc4362f9150d26
Norman Witte III <norm.witte@gmail.com>
2019-03-10 19:58:17 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var forwarder = require("aws-lambda-ses-forwarder");
+const { FROM_EMAIL, TO_EMAIL } = process.env;
+
+exports.handler = function (event, context, callback) {
+  const overrides = {
+    config: {
+      fromEmail: FROM_EMAIL,
+      subjectPrefix: "",
+      emailBucket: "djmeph-email",
+      emailKeyPrefix: `emails/${FROM_EMAIL}/`,
+      forwardMapping: {
+        [FROM_EMAIL]: [TO_EMAIL]
+      }
+    }
+  };
+  forwarder.handler(event, context, callback, overrides);
+};
